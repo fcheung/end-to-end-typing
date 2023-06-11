598a29a519c6e296ed6da557032f050bbd32102f
Frederick Cheung <frederick.cheung@gmail.com>
2023-06-11 16:46:38 +0100

    implement load user

--- app/javascript/components/users.tsx
+++ app/javascript/components/users.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import { GetUsersResponse } from '../generated_types/users'
+import { GetUsersResponse, UserDetail } from '../generated_types/users'
 import PaginationLinks from './pagination'
 import getApi from '../utils/get_api'
+import User from './user'
 
 const Users = ({ pagination: initialPagination, users: initialUsers }: GetUsersResponse) => {
   const [pagination, setPagination] = useState(initialPagination)
   const [users, setUsers] = useState(initialUsers)
+
+  const [loadedUser, setLoadedUser] = useState<UserDetail | undefined>()
 
   // this methods loads the requested page & re-renders
   // the table/pagination using it
@@ -13,6 +16,11 @@ const Users = ({ pagination: initialPagination, users: initialUsers }: GetUsersR
     const response = await getApi(`/users?page=${pageNumber}`)
     setUsers(response.users)
     setPagination(response.pagination)
+  }
+
+  const loadUser = async (userId: number) => {
+    const response = await getApi(`/users/${userId}`)
+    setLoadedUser(response.user)
   }
 
   return (
@@ -30,12 +38,16 @@ const Users = ({ pagination: initialPagination, users: initialUsers }: GetUsersR
             <tr key={u.id}>
               <td>{u.id}</td>
               <td>{u.name}</td>
-              <td>Show</td>
+              <td>
+                <a onClick={() => loadUser(u.id)}>Show</a>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
       <PaginationLinks onPageChanged={newPage => loadPage(newPage)} {...pagination} />
+
+      {loadedUser && <User user={loadedUser} />}
     </>
   )
 }
