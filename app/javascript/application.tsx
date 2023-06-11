1051d6d6e59c8057fc489e3ca9c271f60c2d5364
Frederick Cheung <frederick.cheung@gmail.com>
2023-06-11 16:19:29 +0100

    render users component on page load

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,1 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import { GetUsersResponse } from './generated_types/users'
+import Users from './components/users'
 // Entry point for the build script in your package.json
+
+document.addEventListener('DOMContentLoaded', () => {
+  const usersContainer = document.querySelector("[data-component='users']")
+  if (usersContainer) {
+    const propsData = (usersContainer as HTMLElement).dataset.props
+    if (propsData) {
+      const data = JSON.parse(propsData) as GetUsersResponse
+      const root = createRoot(usersContainer)
+      root.render(<Users {...data} />)
+    }
+  }
+})
